57f4354bef637e4b827831ddfd1c6fe8a52e82bd
JDogMad <oumaima.zerouali@gmail.com>
2022-06-11 21:42:37 +0200

    Database Aangemaakt

---
+++ src/main/java/com/example/voorbeeldexamenv3/VoorbeeldExamenv3Application.java
@@ -0,0 +1,13 @@
+package com.example.voorbeeldexamenv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoorbeeldExamenv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoorbeeldExamenv3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voorbeeldexamenv3/VoorbeeldExamenv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voorbeeldexamenv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoorbeeldExamenv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
